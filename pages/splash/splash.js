f354d13fe170095fcd1ccce0cd46008fa9c65b61
aehyok <aehyok@vip.qq.com>
2016-10-13 09:19:00 +0800

    设置swiper控制高度，通过wx.getSystemInfo小程序Api进行获取手机屏幕高度进行设置

--- pages/splash/splash.js
+++ pages/splash/splash.js
@@ -1,5 +1,6 @@
 Page({
   data: {
+    screenHeight:'0px',
     title: 'aehyok.com',
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
@@ -9,5 +10,15 @@ Page({
   },
   start () {
     wx.navigateTo({ url: '../featured/featured' })
+  },
+  onLoad(){
+    var that=this;  //作用域问题
+    var screenHeight="0px";
+    wx.getSystemInfo({
+      success:function(res){
+          screenHeight=res.windowHeight+"px";
+          that.setData({screenHeight:screenHeight});
+      }
+    });
   }
 })
